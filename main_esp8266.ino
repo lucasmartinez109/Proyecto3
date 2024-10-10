6fbf7656af139d30229f506aadfbb674cef32b58
Juan Francisco Pirez <135382640+juanfrapm@users.noreply.github.com>
2024-10-10 19:31:02 -0300

    Update main_esp8266.ino

--- main_esp8266.ino
+++ main_esp8266.ino
@@ -1,94 +1,153 @@
-#include "secrets.h"
+#include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include "WiFi.h"
+#include <time.h>
+#include "secrets.h"
+#define TIME_ZONE -5
+ 
+ 
+float h = 0;
+float t = 0;
+unsigned long lastMillis = 0;
+unsigned long previousMillis = 0;
+const long interval = 5000;
  
 #define AWS_IOT_PUBLISH_TOPIC   "esp8266/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp8266/sub"
  
-WiFiClientSecure net = WiFiClientSecure();
+WiFiClientSecure net;
+ 
+BearSSL::X509List cert(cacert);
+BearSSL::X509List client_crt(client_cert);
+BearSSL::PrivateKey key(privkey);
+ 
 PubSubClient client(net);
  
-void connectAWS()
+time_t now;
+time_t nowish = 1510592825;
+ 
+ 
+void NTPConnect(void)
 {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
- 
-  Serial.println("Connecting to Wi-Fi");
- 
-  while (WiFi.status() != WL_CONNECTED)
+  Serial.print("Setting time using SNTP");
+  configTime(TIME_ZONE * 3600, 0 * 3600, "pool.ntp.org", "time.nist.gov");
+  now = time(nullptr);
+  while (now < nowish)
   {
     delay(500);
     Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("done!");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
+ 
+ 
+void messageReceived(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Received [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+ 
+ 
+void connectAWS()
+{
+  delay(3000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
+  Serial.println(String("Attempting to connect to SSID: ") + String(WIFI_SSID));
+ 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
   }
  
-  // Configure WiFiClientSecure to use the AWS IoT device credentials
-  net.setCACert(AWS_CERT_CA);
-  net.setCertificate(AWS_CERT_CRT);
-  net.setPrivateKey(AWS_CERT_PRIVATE);
+  NTPConnect();
  
-  // Connect to the MQTT broker on the AWS endpoint we defined earlier
-  client.setServer(AWS_IOT_ENDPOINT, 8883);
+  net.setTrustAnchors(&cert);
+  net.setClientRSACert(&client_crt, &key);
  
-  // Create a message handler
-  client.setCallback(messageHandler);
+  client.setServer(MQTT_HOST, 8883);
+  client.setCallback(messageReceived);
+ 
  
   Serial.println("Connecting to AWS IOT");
  
   while (!client.connect(THINGNAME))
   {
     Serial.print(".");
-    delay(100);
+    delay(1000);
   }
  
-  if (!client.connected())
-  {
+  if (!client.connected()) {
     Serial.println("AWS IoT Timeout!");
     return;
   }
- 
   // Subscribe to a topic
   client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
  
   Serial.println("AWS IoT Connected!");
 }
  
+ 
 void publishMessage()
 {
   StaticJsonDocument<200> doc;
-  //doc["humidity"] = h;
-  //doc["temperature"] = t;
+  doc["time"] = millis();
+  doc["humidity"] = h;
+  doc["temperature"] = t;
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
  
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
  
-void messageHandler(char* topic, byte* payload, unsigned int length)
-{
-  Serial.print("incoming: ");
-  Serial.println(topic);
- 
-  StaticJsonDocument<200> doc;
-  deserializeJson(doc, payload);
-  const char* message = doc["message"];
-  Serial.println(message);
-}
  
 void setup()
 {
   Serial.begin(115200);
   connectAWS();
-  dht.begin();
 }
+ 
  
 void loop()
 {
-  //h = dht.readHumidity();
-  //t = dht.readTemperature();
-  
-  publishMessage();
-  client.loop();
-  delay(1000);
+  h = h + 1;
+  t = t + 1;
+ 
+ 
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+  delay(2000);
+ 
+  now = time(nullptr);
+ 
+  if (!client.connected())
+  {
+    connectAWS();
+  }
+  else
+  {
+    client.loop();
+    if (millis() - lastMillis > 5000)
+    {
+      lastMillis = millis();
+      publishMessage();
+    }
+  }
 }
